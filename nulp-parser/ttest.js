b2c065dd2a2afc37e429046792e8f59e6ddb15b0
gebeto <slavik.nychkalo@gmail.com>
2019-07-08 14:55:24 +0300

    remove test file

--- nulp-parser/ttest.js
+++
@@ -1,9 +0,0 @@
-const getShedule = require('./api/').getShedule
-
-console.log('start')
-getShedule('ПІ-41').then(function(shedule) {
-// getShedule('ПЗ-31з').then(function(shedule) {
-	console.log(shedule)
-	// console.log('res', shedule);
-	// console.log('day', shedule[0].items[0]);
-});
