9ef5bf91814e42f312cafd4ac26ad95e99a744b4
gebeto <slavik.nychkalo@gmail.com>
2019-09-10 15:22:40 +0300

    remove unused code

--- nulp-parser/server/calendar.js
+++ nulp-parser/server/calendar.js
@@ -53,39 +53,3 @@ function createSchedule(title, schedule) {
 exports.formatLesson = formatLesson;
 exports.createDay = createDay;
 exports.createSchedule = createSchedule;
-
-
- 
-// ics.createEvent(event, (error, value) => {
-//   if (error) {
-//     console.log(error)
-//     return
-//   }
- 
-//   console.log(value)  
-//   // BEGIN:VCALENDAR
-//   // VERSION:2.0
-//   // CALSCALE:GREGORIAN
-//   // PRODID:adamgibbons/ics
-//   // METHOD:PUBLISH
-//   // X-PUBLISHED-TTL:PT1H
-//   // BEGIN:VEVENT
-//   // UID:d9e5e080-d25e-11e8-806a-e73a41d3e47b
-//   // SUMMARY:Bolder Boulder
-//   // DTSTAMP:20181017T204900Z
-//   // DTSTART:20180530T043000Z
-//   // DESCRIPTION:Annual 10-kilometer run in Boulder\, Colorado
-//   // URL:http://www.bolderboulder.com/
-//   // GEO:40.0095;105.2669
-//   // LOCATION:Folsom Field, University of Colorado (finish line)
-//   // STATUS:CONFIRMED
-//   // CATEGORIES:10k races,Memorial Day Weekend,Boulder CO
-//   // ORGANIZER;CN=Admin:mailto:Race@BolderBOULDER.com
-//   // ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN=Adam Gibbons:mailto:adam@example.com
-//   // ATTENDEE;RSVP=FALSE;ROLE=OPT-PARTICIPANT;DIR=https://linkedin.com/in/brittanyseaton;CN=Brittany
-//   //   Seaton:mailto:brittany@example2.org
-//   // DURATION:PT6H30M
-//   // END:VEVENT
-//   // END:VCALENDAR
-// })
-
